67d89bef38deb282895811d90d95627f87045e2b
Unknown <awangku.safwan92@gmail.com>
2019-04-11 17:13:43 +0800

    Add read by chunk
    chunk large file to smaller file

--- read.py
+++ read.py
@@ -1,19 +1,31 @@
-# this is a function to read large file and split to chunks\
+# this is a function to read large file and split to chunks
+# use splitfile() for txt - by size
+# use chunk()  for csv    - by row
 
 import pandas as pd
 import os
 
-def read_csv():
-    root = "C:\\Users\\User\\Desktop\\Python Tutorial\\03_Reading files\\"
+def read_file():
+    # root = "C:\\Users\\User\\Desktop\\Python Tutorial\\03_Reading files\\"
+	# dataPath = root+"dataFile\\Comma-Separated.txt"
+
+    # GET PATH
+    # os.getcwd() #get current working directory
+    dataPath = os.path.join(os.getcwd(), "dataFile\\Comma-Separated.txt") # 758 lines include header
+    processedPath = os.path.join(os.getcwd(), "processedData\\")
+    # print(type(os.getcwd())) # type() - to get datatype
 
     # create data_frame
-    # dataPath = root+"dataFile\\Comma-Separated.txt"
-    dataPath = os.path.join(os.getcwd(), "dataFile\\Comma-Separated.txt")
-    print(type(os.getcwd()))
     # This is when data dont have header, Default is have header
     # df = pd.read_csv(root+dataPath, header=None) 
-    df = pd.read_csv(dataPath)
-    print(df.head()) # can specify number of column in df.head(n)
+    # df = pd.read_csv(dataPath)
+    # print(df.head()) # can specify number of row(s) in df.head(n)
+
+    chunksize = 200 # by row
+    file_name = 0
+    for chunk in pd.read_csv(dataPath, error_bad_lines=False, dtype={'columname':str}, chunksize=chunksize):
+        file_name = chunksize + file_name
+        chunk.to_csv(processedPath+ str(file_name) + '_file.txt', index=False, mode='w')
 
     return
 
@@ -21,8 +33,8 @@ def read_csv():
 
 # main method
 def main():
-    data = read_csv()
-    #read_csv()
+    # data = read_file()
+    read_file()
     # mapping()
     # indexing(data)
 
